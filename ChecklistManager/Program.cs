27d11309b2db2b916b1b9fa4e0630083731dfe7a
PetersonE1 <PetersonE_1@live.com>
2025-01-13 11:28:09 -0500

    enabled swagger on release build because tbh it's a pretty nice way to manage data

--- ChecklistManager/Program.cs
+++ ChecklistManager/Program.cs
@@ -43,11 +43,11 @@ namespace ChecklistManager
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            //}
 
             app.UseHttpsRedirection();
 
